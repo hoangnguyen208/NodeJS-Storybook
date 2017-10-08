30329525e7fdc08a4f83955d78e37215a6252d8c
unknown <hoangnguyen.vn208@gmail.com>
2017-10-08 17:32:35 +0300

    handle auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://hoang:123456@ds113795.mlab.com:13795/node-storybooks',
+  googleClientID: '658737869322-lv6p066ocedag9jlslsf9krtnuqegbgr.apps.googleusercontent.com',
+  googleClientSecret: 'TiGP2_te7piYgCNsDQuSXL2q'
+}
